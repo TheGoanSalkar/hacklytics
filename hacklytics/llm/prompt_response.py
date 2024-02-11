1dec443852a0d05cb95b98a29fe69c1201140ffc
vchabria <96632514+vchabria@users.noreply.github.com>
2024-02-11 05:07:58 -0500

    Delete hacklytics directory

--- hacklytics/llm/prompt_response.py
+++
@@ -1,6 +0,0 @@
-class PromptResponse():
-    def __init__(self, immediate_issues, immediate_issue_fixes, longterm_issues, longterm_issue_fixes):
-        self.immediate_issues = immediate_issues
-        self.immediate_issue_fixes = immediate_issue_fixes
-        self.longterm_issues = longterm_issues
-        self.longterm_issue_fixes = longterm_issue_fixes
--- hacklytics/settings.py
+++
@@ -1,5 +0,0 @@
-import os
-from openai import OpenAI
-
-OPENAI_KEY = os.environ.get('OPENAI_KEY')
-client = OpenAI(api_key=OPENAI_KEY)
